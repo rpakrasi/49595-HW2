0d0dfc0c892eec6103f61d7199f43c333187837a
Kiara Chau Garcia <kchaugarcia@student.ethz.ch>
2026-04-14 23:47:11 -0400

    Add deterministic controller

---
+++ deterministic-controller/duckiebot_controller_v2.py
@@ -0,0 +1,91 @@
+import time
+
+import pyautogui
+
+keyboard_set = {"w", "a", "s", "d", "z", "x"}  # Keys we will use for control
+
+
+class DuckiebotKeyboardControllerV2:
+    def __init__(self, gain=0.1):
+        self.gain = gain
+
+    def turn_left_long_curve(self):
+        self._turn_long_curve("a")
+
+    def turn_right_long_curve(self):
+        self._turn_long_curve("d")
+
+    def circle(self, key):
+        # TODO go in a circle with 55cm radius
+        self._release_all()
+        self.set_gain(0.4)
+        pyautogui.keyDown("w")
+        print("Moving in circles.")
+        for _ in range(18):  # TODO adjust
+            pyautogui.keyDown(key)
+            time.sleep(0.4)  # TODO adjust
+            pyautogui.keyUp(key)
+            time.sleep(0.7)  # TODO adjust
+
+    def turn_left(self):
+        self._turn_short_curve("a")
+
+    def turn_right(self):
+        self._turn_short_curve("d")
+
+    def drive_forward(self):
+        self._release_all()
+        pyautogui.keyDown("w")
+        print("Moving forward.")
+
+    def drive_backward(self):
+        self._release_all()
+        pyautogui.keyDown("s")
+        print("Moving backwards.")
+
+    def set_gain(self, new_gain):
+        old_gain = self.gain
+        steps = int((new_gain - old_gain) * 10)
+        if steps > 0:
+            for _ in range(steps):
+                pyautogui.press("x")
+                self.gain += 0.1
+        elif steps < 0:
+            for _ in range(abs(steps)):
+                pyautogui.press("z")
+                self.gain -= 0.1
+        print(f"Changing gain from {old_gain:.2f} to {self.gain:.2f}")
+
+    def emergency_stop(self):
+        self._release_all()
+
+    @staticmethod
+    def _release_all():
+        for key in keyboard_set:
+            pyautogui.keyUp(key)
+
+    def _turn_long_curve(self, key):
+        # TODO should be 90 degrees
+        self._release_all()
+        self.set_gain(0.4)
+        pyautogui.keyDown("w")
+        pyautogui.keyDown(key)
+        time.sleep(0.9)
+        pyautogui.keyUp(key)
+        print("Turning long curve.")
+
+    def _turn_short_curve(self, key):
+        # TODO should be 45 degrees
+        self._release_all()
+        self.set_gain(0.4)
+        pyautogui.keyDown("w")
+        pyautogui.keyDown(key)
+        time.sleep(0.45)
+        pyautogui.keyUp(key)
+        print("Turning short curve.")
+
+    def turn_tiny(self, key):
+        pyautogui.keyDown(key)
+        time.sleep(0.15)
+        pyautogui.keyUp(key)
+        print("Turning very shortly.")
---
+++ deterministic-controller/main.py
@@ -0,0 +1,87 @@
+import signal
+import time
+
+import speech_to_text_microsoft
+from duckiebot_controller_v2 import DuckiebotKeyboardControllerV2
+
+gain = 0.1
+controller = DuckiebotKeyboardControllerV2(gain=gain)
+done = False
+
+
+def execute_intent(intent: str):
+    intent = intent.lower()
+    if "stop" in intent:
+        controller.emergency_stop()
+    elif "straight" in intent or "forward" in intent:
+        controller.drive_forward()
+    elif "backward" in intent:
+        controller.drive_backward()
+    elif "turn left" in intent:
+        controller.turn_left_long_curve()
+    elif "turn right" in intent:
+        controller.turn_right_long_curve()
+    elif "turn left short" in intent:
+        controller.turn_left()
+    elif "turn right short" in intent:
+        controller.turn_right()
+    elif "left circle" in intent:
+        controller.circle("a")
+    elif "right circle" in intent:
+        controller.circle("d")
+    elif "left" in intent:
+        controller.turn_tiny("a")
+    elif "right" in intent:
+        controller.turn_tiny("d")
+    elif "super speed" in intent:
+        controller.set_gain(2)
+    elif "fastest" in intent:
+        controller.set_gain(1.6)
+    elif "faster" in intent:
+        controller.set_gain(1.3)
+    elif "fast" in intent:
+        controller.set_gain(1)
+    elif "slowest" in intent:
+        controller.set_gain(0.1)
+    elif "slower" in intent:
+        controller.set_gain(0.3)
+    elif "slow" in intent:
+        controller.set_gain(0.6)
+
+
+def process_utterance(said):
+    global done
+    print("Processing utterance: ", said)
+    if "bye" in said.lower() or "exit" in said.lower():
+        done = True
+    else:
+        execute_intent(said)
+
+
+def main():
+    global done
+    done = False
+
+    def signal_handler(sig, frame):
+        global done
+        done = True
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print(f"Starting. Gain set to: {gain}")
+
+    try:
+        speech_to_text_microsoft.process_utterance = process_utterance
+        speech_to_text_microsoft.start()
+
+        while not done:
+            time.sleep(0.05)
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+    finally:
+        speech_to_text_microsoft.stop()
+
+
+if __name__ == "__main__":
+    main()
